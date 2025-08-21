0f8569db5fad6a1655ee6f36843888cdb69d73ae
TassnimP <tassnim.papleux@student.ehb.be>
2025-08-21 02:14:06 +0200

    searchbar

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 const container = document.getElementById("characters-container");
+const searchInput = document.getElementById("searchInput");
 
-fetch("https://rickandmortyapi.com/api/character")
-.then(res => res.json())
-.then(data => {
-    data.results.forEach(character => {
+let allCharacters = [];
+
+async function fetchAllCharacters() {
+    let allData = [];
+    let nextUrl = 'https://rickandmortyapi.com/api/character';
+
+    while (nextUrl) {
+        const res = await fetch(nextUrl);
+        const data = await res.json();
+        console.log("Fetching page:", nextUrl);
+        allData = [...allData, ...data.results];
+        nextUrl = data.info.next;
+    }
+    console.log("Total characters fetched:", allData.length);
+    allCharacters = allData;
+    displayCharacters(allCharacters);
+}
+
+function displayCharacters(characters) {
+    container.innerHTML = '';
+
+    characters.forEach(character => {
         const card = document.createElement("div");
         card.className = "character-card";
         card.innerHTML = `
         <h3>${character.name}</h3>
         <img src="${character.image}" alt="${character.name}">
         <p>Status: ${character.status}</p>
-        <p>Species: ${character.species}</p>
-        `;
+        <p>Species: ${character.species}</p>`;
+
         container.appendChild(card);
     });
-})
-.catch(err => {
-    container.innerHTML =`<p>Error loading characters.</p>`;
-    console.error(err);
+}
+searchInput.addEventListener("input", (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filtered = allCharacters.filter(character =>
+        character.name.toLowerCase().includes(searchTerm)
+    );
+    displayCharacters(filtered);
 });
+
+fetchAllCharacters();
